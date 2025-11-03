6a917e71e7977886f27cc38c9dfd5cec059b33e2
Artemiy Ivanov <ivan0v.a.a@mail.ru>
2025-11-03 15:50:07 +0300

    сделал 1 и 2 задаиня

--- fan_in/merge_channels.go
+++ fan_in/merge_channels.go
@@ -1,9 +1,35 @@
 package fan_in
 
+import (
+	"sync"
+)
+
 // MergeChannels - принимает несколько каналов на вход и объединяет их в один
 // Fan-in и merge channels синонимы
 func MergeChannels(channels ...<-chan int) <-chan int {
-	// TODO: ваш код
+	kol_chan := len(channels)
 
-	panic("implement me")
+	var wg sync.WaitGroup
+
+	res_chan := make(chan int)
+
+	wg.Add(kol_chan)
+
+	go func() {
+		wg.Wait()
+		close(res_chan)
+	}()
+
+	for i := 0; i < kol_chan; i++ {
+		go func(index int) {
+			defer wg.Done()
+
+			for v := range channels[index] {
+				res_chan <- v
+			}
+		}(i)
+
+	}
+
+	return res_chan
 }
--- generate_password/generate_password.go
+++ generate_password/generate_password.go
@@ -1,10 +1,40 @@
 package generate_password
 
-// letters - список допустимых символов в пароле
+import (
+	"crypto/rand"
+	"fmt"
+)
+
 const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func GeneratePassword(n int) (string, error) {
-	// TODO: ваш код
+	if n == 0 {
+		return "", nil
+	}
+	if n <= 0 {
+		return "", fmt.Errorf("длина пароля должна быть больше 0")
+	}
 
-	panic("implement me")
+	bytes := make([]byte, n)
+	lettersLen := byte(len(letters))
+
+	for i := 0; i < n; i++ {
+		var b [1]byte
+		_, err := rand.Read(b[:])
+		if err != nil {
+			return "", fmt.Errorf("ошибка генерации случайного числа: %v", err)
+		}
+		bytes[i] = letters[b[0]%lettersLen]
+	}
+
+	return string(bytes), nil
 }
+
+// Что можно улучшить:
+// 1. добавить crypto/rand так, чтобы каждый символ выбирался с равной вероятностью, без остатка от деления.
+// 2. хранить историю сгенерированных паролей, чтобы не допустить повторений
+
+// Можно ли угадать генерируемую строку?
+// - Если используется crypto/rand, угадать пароль практически невозможно.
+// - Даже если человек знает алгоритм и набор символов, для длины 16 символов вероятность подобрать пароль случайно: 4.77*10^−29
+// - но возможны коллизии 
